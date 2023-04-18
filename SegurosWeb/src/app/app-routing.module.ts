0a794a8f5ba8946335e8bca91dec6a485676aa3e
Oscar Escobar <qomc16@gmail.com>
2023-04-18 11:32:13 -0600

    Version 0.2.1

--- SegurosWeb/src/app/app-routing.module.ts
+++ SegurosWeb/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CrearClienteComponent } from './crear-cliente/crear-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'crearCliente', component:CrearClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SegurosWeb/src/app/app.module.ts
+++ SegurosWeb/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { CrearClienteComponent } from './crear-cliente/crear-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    HomeComponent,
+    CrearClienteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
